e19867668c07953ca76e2e4222a480d44cf2fb2f
Diana Kovacheva <dmarinova@outlook.com>
2024-07-23 15:23:29 +0200

    Solve the problems from problem set 7

---
+++ 07-Week-7/01-Problem-Set-7/songs/7.sql
@@ -0,0 +1,5 @@
+/* write a SQL query that returns the average energy of songs that are by Drake. */
+
+SELECT AVG(energy) FROM songs AS s
+JOIN artists AS a ON s.artist_id == a.id
+WHERE a.name = "Drake";
